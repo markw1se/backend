22de2defc093a85e8cb02343699277fe0c7f20f4
eakyoungyou <eakyoungyu@gmail.com>
2023-09-09 01:28:37 +0900

    Signup with firebaseAuth

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,44 +3,21 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const firebase = require('../../firebase');
 
 exports.user_signup = (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: "Mail exists"
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash
-            });
-            user
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created"
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
+  const firebaseAuth = firebase.firebaseAuth;
+  email = req.body.email
+  password = req.body.password
+  firebase.createUserWithEmailAndPassword(firebaseAuth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      res.status(201).json({ message: "User created successfully", user });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      res.status(500).json({ error: errorMessage });
     });
 };
 
